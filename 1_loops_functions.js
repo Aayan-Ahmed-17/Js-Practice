9d427e9a6a773d74699e689ed9001f3c67294189
Aayan Ahmed <aayanahmedtejani@gmail.com>
2024-09-27 12:59:52 +0500

    created html & js file and 2 question completed

---
+++ 1_loops_functions.js
@@ -0,0 +1,22 @@
+/*
+*Loops Questions:
+
+?01 Write a for loop that prints the numbers from 1 to 10.
+?02 Create a while loop that prints the even numbers between 0 and 20.
+*/
+
+//---------------Loops Ans----------------
+
+//* 01
+for (let i = 1; i <= 10; i++) {
+    //console.log(i)
+}
+
+//* 02
+let num = 1;
+while (num <= 20) {
+    if (!(num % 2)) {
+        // console.log(num)
+    }
+    num++;
+}
