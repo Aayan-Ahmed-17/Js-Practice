cdf537f8b79e6b06a23af4a958c2141801b82a14
Aayan Ahmed <aayanahmedtejani@gmail.com>
2024-10-10 23:14:39 +0500

    array destructure q8 - q10

--- 7_destructuring.js
+++ 7_destructuring.js
@@ -7,6 +7,10 @@
 ?04 Destructure array with default values for missing elements
 ?05 Extract the second element from the first sub-array
 ?06 Swap x and y using destructuring
+?07 Function Return Values
+?08 Extract 3 and 6 from the nested structure
+?09 Transform into objects using destructuring in map
+?10 Extract specific numbers and the function
 */
 
 // ==========================Ans=====================
@@ -35,3 +39,37 @@ let x = 5;
 let y = 10;
 [x, y] = [y, x] //* done
 
+//* 07
+function getCoordinates() {
+    return [50, 100];
+}
+const [xCordinate, yCordinate] = getCoordinates() //* done
+
+//* 08 
+const data = [1, [2, 3], 4, [5, [6, 7]]];
+const [, [, elem1], , [, [elem2]]] = data //* done
+
+//* 09 
+const users = [
+    ['John', 'Doe', 30],
+    ['Jane', 'Smith', 25],
+];
+const formattedUsers = users.map(([firstName, lastName, age]) => ({
+    firstName,
+    lastName,
+    age
+})); //* done
+
+//* 10
+function processData() {
+    return [
+        [[1, 2], [3, 4]],
+        [[5, 6], [7, 8]],
+        function(a, b) { return a + b; }
+    ];
+}
+const [
+    [[,num2], [,num4]], 
+    [,[,num8]], 
+    addFunc
+] = processData() //* done
